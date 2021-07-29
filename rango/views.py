076c7e636fdbece711d35f686672d83ac357838e
Yujie Li <2540477l@student.gla.ac.uk>
2021-07-29 22:50:03 +0100

    third commit

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+#def index(request):
+#    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+#def about(request):
+#    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    # Spoiler: you don't need to pass a context dictionary here.
+    return render(request, 'rango/about.html')
